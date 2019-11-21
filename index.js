67fbe07a0766a22c8af7436f19ab84c9f89075ae
Harvey Le <harveyxcle@gmail.com>
2019-11-20 20:45:58 -0500

    emptied input area after submit

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ $(function() {
 
         event.preventDefault();
 
+        // saving value of input as userText
         const userText = $(event.currentTarget).find('#shopping-list-entry').val();
 
+        // empties out the input box after an item is submitted
+        $('#shopping-list-entry').val('');
+
+        // adding item to the bottom of the shopping list in li format
         $('.shopping-list').append(`
         <li>
             <span class="shopping-item">${userText}</span>
@@ -19,10 +24,12 @@ $(function() {
         </li>`);
     });
 
+    // adding strikethrough class to span when check button is clicked
     $('ul').on('click', '.shopping-item-toggle', function(event) {
         $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
     });
     
+    // deleting whole list item when delete button is clicked
     $('ul').on('click', '.shopping-item-delete', function(event) {
         $(this).closest('li').remove();
     });
